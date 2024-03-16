69d507ec6f23db1e6c66c9d700553bbc1b03ce6f
honeypatkar70 <honeypatkar70@gmail.com>
2024-03-16 13:10:03 +0530

    Making Image gallery app using react-image-gallery

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Image from './components/Image'
+
+function App() {
+
+  return (
+   <>
+   <h1>Image Gallery</h1>
+   <Image/>
+   </>
+  )
+}
+
+export default App
