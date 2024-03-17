27036a15dcafdc283143c60c50e8657f90815058
honeypatkar70 <honeypatkar70@gmail.com>
2024-03-17 15:48:09 +0530

    Making a diffrent files for images

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,55 +1,13 @@
 import React from 'react';
-import ImageGallery from 'react-image-gallery';
+import Gallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
+import images from "./imageData"
 
 export default function Image() {
 
-  const images = [
-    {
-      original: "https://picsum.photos/id/1018/1000/600/",
-      thumbnail: "https://picsum.photos/id/1018/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1015/1000/600/",
-      thumbnail: "https://picsum.photos/id/1015/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1010/1000/600/",
-      thumbnail: "https://picsum.photos/id/1010/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1050/1000/600/",
-      thumbnail: "https://picsum.photos/id/1050/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1025/1000/600/",
-      thumbnail: "https://picsum.photos/id/1025/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1000/1000/600/",
-      thumbnail: "https://picsum.photos/id/1000/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1002/1000/600/",
-      thumbnail: "https://picsum.photos/id/1002/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1020/1000/600/",
-      thumbnail: "https://picsum.photos/id/1020/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1006/1000/600/",
-      thumbnail: "https://picsum.photos/id/1006/250/150/",
-    },
-  ];
-
   return (
     <div>
-      <ImageGallery items={images}/>
+      <Gallery items={images}/>
     </div>
   );
 }
---
+++ src/components/imageData.js
@@ -0,0 +1,68 @@
+const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1010/1000/600/",
+      thumbnail: "https://picsum.photos/id/1010/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1050/1000/600/",
+      thumbnail: "https://picsum.photos/id/1050/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1025/1000/600/",
+      thumbnail: "https://picsum.photos/id/1025/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1000/1000/600/",
+      thumbnail: "https://picsum.photos/id/1000/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1002/1000/600/",
+      thumbnail: "https://picsum.photos/id/1002/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1020/1000/600/",
+      thumbnail: "https://picsum.photos/id/1020/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1006/1000/600/",
+      thumbnail: "https://picsum.photos/id/1006/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1051/1000/600/",
+      thumbnail: "https://picsum.photos/id/1051/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1024/1000/600/",
+      thumbnail: "https://picsum.photos/id/1024/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1009/1000/600/",
+      thumbnail: "https://picsum.photos/id/1009/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1003/1000/600/",
+      thumbnail: "https://picsum.photos/id/1003/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1056/1000/600/",
+      thumbnail: "https://picsum.photos/id/1056/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1040/1000/600/",
+      thumbnail: "https://picsum.photos/id/1040/250/150/",
+    },
+  ];
+
+  export default images;
